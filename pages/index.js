5b44b91d696d01bdea1713eb398cb2ce7ee52369
karl@karlfriman.com <karl@karlfriman.com>
2021-11-08 17:56:06 +0100

    enum

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,18 @@ import axios from 'axios';
 import ReactPaginate from 'react-paginate';
 import Router, { withRouter } from 'next/router';
 
+const films = {
+  1: 'https://swapi.dev/api/films/1/',
+  2: 'https://swapi.dev/api/films/2/',
+  3: 'https://swapi.dev/api/films/3/',
+  4: 'https://swapi.dev/api/films/4/',
+  5: 'https://swapi.dev/api/films/5/',
+  6: 'https://swapi.dev/api/films/6/',
+  7: 'https://swapi.dev/api/films/7/',
+  8: 'https://swapi.dev/api/films/8/',
+  9: 'https://swapi.dev/api/films/9/',
+};
+
 const Home = (props) => {
   const [isLoading, setLoading] = useState(false);
   const startLoading = () => setLoading(true);
@@ -34,8 +46,18 @@ const Home = (props) => {
   else {
     content = (
       <ul>
-        {props.posts.map((post, id) => {
-          return <li key={id}>{post.name}</li>;
+        {props.posts.map((character, id) => {
+          let films = '';
+
+          character.films.map((film) => {
+            films += film;
+          });
+
+          return (
+            <li key={id} className={character}>
+              {character.name} : {films}
+            </li>
+          );
         })}
       </ul>
     );
@@ -80,7 +102,7 @@ Home.getInitialProps = async ({ query }) => {
   const returnsPerPage = 10;
   const page = query.page || 1;
   const posts = await axios.get(`https://swapi.dev/api/people?page=${page}`);
-  console.log(posts.data.next);
+
   return {
     totalCount: posts.data.count,
     pageCount: posts.data.count / returnsPerPage,
